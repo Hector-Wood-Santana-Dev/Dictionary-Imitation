95a32fd6f3720287a8e575a854cf70864081fd91
Marcos <marcosmr04@gmail.com>
2024-03-24 10:50:32 +0100

    Class TestTwoElementDictionary, created test: testLenAndSize,testGetDictionaryValue,testInsertElementValid,testInsertElementNull,testDeleteElementNull,testDeleteElementValid, testPopValue.

--- src/Tests/TestTwoElementsDictionary.java
+++ src/Tests/TestTwoElementsDictionary.java
@@ -6,29 +6,31 @@ import Dictionary.*;
 import static java.lang.Boolean.*;
 
 /**
- * Test Class for a one item on dictionary.
+ * Test Class for two elements on dictionary.
  */
-public class TestOneElementDictionary {
+
+public class TestTwoElementsDictionary {
     private Dictionary dictionary;
 
     /**
      * <pre>
-     * Initializing the table with 1 element.
-     * The table length must be 10 (9 null elements, 1 valid element).
+     * Initializing the table with 2 elements.
+     * The table length must be 10 (9 null elements, 2 valid element).
      * </pre>
      */
     @Before
     public void init(){
         dictionary = new Dictionary();
         dictionary.insertElement("Food", 3);
+        dictionary.insertElement("Drink", 2);
     }
 
     /**
      * <pre>
      * Test if the dictionary initializes correctly.
      * The length of the dictionary must be 10.
-     * The size of the dictionary must be 1.
-     * Because there is one element inside the dictionary.
+     * The size of the dictionary must be 2.
+     * Because there is two elements inside the dictionary.
      * </pre>
      */
     @Test
@@ -36,17 +38,16 @@ public class TestOneElementDictionary {
         int lenDictionary = dictionary.lenDictionary();
         int sizeDictionary = dictionary.sizeDictionary();
         assertEquals("The length must be 10.", 10, lenDictionary);
-        assertEquals("The size must be 1.", 1, sizeDictionary);
+        assertEquals("The size must be 2.", 2, sizeDictionary);
     }
-
     /**
      * Test that verifies that given a key returns its attributed value.
      */
     @Test
     public void testGetDictionaryValue() {
         assertEquals("The value of the key 'Food' must be 3.", 3, dictionary.getDictionaryValue("Food"));
+        assertEquals("The value of the key 'Drink' must be 2.", 2, dictionary.getDictionaryValue("Drink"));
     }
-
     /**
      * <pre>
      * Test that insert a valid element.
@@ -55,10 +56,9 @@ public class TestOneElementDictionary {
      */
     @Test
     public void testInsertElementValid() {
-        dictionary.insertElement("Bacon", "egg");
-        assertEquals("The value of the key 'Bacon' must be 'egg'.", "egg", dictionary.getDictionaryValue("Bacon"));
+        dictionary.insertElement("Apple", "egg");
+        assertEquals("The value of the key 'Apple' must be 'egg'.", "egg", dictionary.getDictionaryValue("Apple"));
     }
-
     /**
      * <pre>
      * Test that insert a null element.
@@ -91,20 +91,21 @@ public class TestOneElementDictionary {
      */
     @Test
     public void testDeleteElementValid() {
-        dictionary.deleteElement("Bacon");
-        assertNull("The value of the key 'Bacon' does not exist.", dictionary.getDictionaryValue("Bacon"));
+        dictionary.deleteElement("Apple");
+        assertNull("The value of the key 'Apple' does not exist.", dictionary.getDictionaryValue("Bacon"));
     }
-
     /**
      * <pre>
      * Test that pops a value given a key.
      * </pre>
      */
     @Test
-    public void testPopValue(){
+    public void testPopValue() {
         Object var = dictionary.popDictionaryValue("Food");
-        assertEquals("The value of the pop should be 3",3, var);
-        assertEquals("The size of the dictionary should be 0",0, dictionary.sizeDictionary());
+        assertEquals("The value of the pop should be 3", 3, var);
+        assertEquals("The size of the dictionary should be 1", 1, dictionary.sizeDictionary());
+        Object var1 = dictionary.popDictionaryValue("Drink");
+        assertEquals("The value of the pop should be 2", 2, var1);
+        assertEquals("The size of the dictionary should be 0", 0, dictionary.sizeDictionary());
     }
-
 }
